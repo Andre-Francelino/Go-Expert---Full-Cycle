129f3a4773eb9fa98d5c42c1a18e538b721d1b33
Andre Francelino <andrefgomes.github.version@gmail.com>
2026-06-06 18:55:19 -0300

    Aula Declaração e Atribuião de variáveis

--- 2/main.go
+++ 2/main.go
@@ -6,14 +6,22 @@ package main
 const a = "Olá mundo!"
 
 // var + nome da variavel + tipo da variavel
+// escopo global
 var b bool
 var c bool
 
+// escopo global
 var (
 	x int
 	y int
 	z float64
 	nome string = "Goku"
+
+	// inferência de valor
+	f int
+	g string
+	h float32
+
 )
 
 func main() {
@@ -29,4 +37,21 @@ func main() {
 	println(y)
 	println(z)
 	println(nome)
+	
+	// inferência de valor
+	println("inferência de um valor inicial mesmo a variável estando supostamente vazia:")
+	println(f)
+	println(g)
+	println(h)
+
+	// escopo local (declaração dentro da função main)
+	var loc string
+	print(loc)
+
+	arquivo := "X" //string
+	println(arquivo)
+	// arquivo := "XXX" Só pode usar os 2 pontos (:) na primeira vez que está declarando a variável
+	arquivo = "XXX" // se for atribuir um novo valor de uma variável já criada já se atribui utlizando somente o igual (=)
+	println(arquivo)
+	
 }
